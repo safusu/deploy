f1925d6260a3330b2aea06cb00fba4a2c06b74ed
Jeren Suzuki <admin@safusu.com>
2016-01-21 22:19:30 -0800

    First draft

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+# Basic updating and program sourcing
 apt-get update
 apt-get dist-upgrade -y
-apt-get curl nano screen wget 
+apt-get install curl nano screen wget git-core language-pack-en
+apt-get autoremove
+
+# Setting up preferences
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+echo "alias ll='ls -FGlAhp'" > ~/.bashrc
+
+# Setting up users
+useradd -m -p $(openssl passwd -1 me) me
+adduser me sudo
+
+
