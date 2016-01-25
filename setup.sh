974cc88d3b1d168c81433f2c418af0bc54927333
Jeren Suzuki <safusu@gmail.com>
2016-01-25 15:21:21 -0800

    Add terminal colors

--- setup.sh
+++ setup.sh
@@ -16,4 +16,13 @@ $USER=me
 useradd -m -p $(openssl passwd -1 $PASS) $USER
 adduser me sudo
 
+#Terminal preferences
+echo "export TERM="xterm-color"" >> /root/.bashrc
+echo "export PS1='\[\e[1;31m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '" >> /root/.bashrc
 
+## change [\e[1;31m\] to other colors - this one is bold red
+##
+## red = \[\e[0;31m\]
+## bold red (style 1) = \[\e[1;31m\]
+## clear coloring = \[\e[0m\]
+##
